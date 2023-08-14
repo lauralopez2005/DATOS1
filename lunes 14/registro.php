5e99c7bfbd07429786b7d1db2020ae8ccd1dc73d
lauralopez2005 <135733927+lauralopez2005@users.noreply.github.com>
2023-08-14 11:58:11 -0500

    Add files via upload

---
+++ lunes 14/registro.php
@@ -0,0 +1,39 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+$servername = "localhost";
+$username = "root";
+$password="";
+$dbname ="registro";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){
+    die("conexion fallida ". $conn->connect_error);
+}
+
+$nombre= $_POST['nombre'];
+$apellido=$_POST['apellido'];
+$cedula=$_POST['cedula'];
+
+$sql = "INSERT INTO datos(
+    nombre, 
+    apellido,
+    cedula
+)
+
+VALUES(
+    '$nombre',
+    '$apellido',
+    '$cedula'
+)";
+
+if ($conn-> query($sql)  !== TRUE) {
+    echo "error al registar " .$conn->error;
+    exit;
+}
+
+echo "Registro exitoso";
+
+ $conn -> close();
+}
+?>
